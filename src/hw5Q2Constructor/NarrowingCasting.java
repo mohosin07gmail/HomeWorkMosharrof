53ab9b5cffaa103fa49b1f0cc4fcc0645df290f1
mohosin07gmail <mohosin07@gmail.com>
2023-12-27 14:01:04 -0500

    widening casting

--- src/hw5Q2Constructor/NarrowingCasting.java
+++ src/hw5Q2Constructor/NarrowingCasting.java
@@ -2,4 +2,17 @@ package hw5Q2Constructor;
 
 public class NarrowingCasting {
 
+	public static void main(String[] args) {
+
+		double myGrade = 3.45874552211122;
+		System.out.println("What is my Grade? Ans: " + myGrade);
+
+		int newMyGrade = (int) myGrade;
+		System.out.println("What is my Grade? Ans: " + newMyGrade);
+
+		byte byteMyGrade = (byte) myGrade;
+		System.out.println("What is my Grade? Ans: " + byteMyGrade);
+
+	}
+
 }
---
+++ src/hw5Q2Constructor/WideningCasting.java
@@ -0,0 +1,14 @@
+package hw5Q2Constructor;
+
+public class WideningCasting {
+
+	public static void main(String[] args) {
+
+		int myBankBalance = 63;
+		System.out.println("My bank balance:" + myBankBalance);
+
+		double doubleMyBankBalance = myBankBalance;
+		System.out.println("My new bank balance: " + doubleMyBankBalance);
+	}
+
+}
