0d1cb1c6e82d630dd6211de5b74a7ab2e2f4c9d5
mohosin07gmail <mohosin07@gmail.com>
2023-12-27 13:45:51 -0500

    formated

--- src/hw5Q2Constructor/Student.java
+++ src/hw5Q2Constructor/Student.java
@@ -1,13 +1,13 @@
 package hw5Q2Constructor;
 
 public class Student {
-	
+
 	public String stName;
 	public int stId;
 	public char sex;
 	public boolean isProgrammer;
 	public float grade;
-	
+
 	public Student() {
 		System.out.println("This is from the default Constructor of the Student class");
 	}
@@ -18,11 +18,8 @@ public class Student {
 		this.sex = sex;
 		this.isProgrammer = isProgrammer;
 		this.grade = grade;
-		System.out.println("Student Name: " + stName + "\tId: " + stId + "\tGender: " + sex + "\tGrade: " + grade + "\tJava Programmer: " + isProgrammer);
+		System.out.println("Student Name: " + stName + "\tId: " + stId + "\tGender: " + sex + "\tGrade: " + grade
+				+ "\tJava Programmer: " + isProgrammer);
 	}
-	
-	
-	
-	
 
 }
--- src/hw5Q2Constructor/StudentTest.java
+++ src/hw5Q2Constructor/StudentTest.java
@@ -1,9 +1,9 @@
 package hw5Q2Constructor;
 
 public class StudentTest {
-	
+
 	public static void main(String[] args) {
-		
+
 		Student student = new Student();
 		Student mosharrof = new Student("Mosharrof", 852, 'M', true, 3.58f);
 	}
